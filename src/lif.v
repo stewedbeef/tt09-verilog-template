3cb3f38767996d44933697c5d24e8f105dcf4033
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2024-11-09 17:00:54 -0800

    LIF

---
+++ src/lif.v
@@ -0,0 +1,30 @@
+module lif(
+    input clk,
+    input [7:0] I, // Input current
+    input rst_n,
+	input en,
+    output spike
+    );
+
+    parameter threshold = 8'd239;
+    reg [7:0] U; // Neuron potential
+    reg [3:0] timer;
+    
+    assign spike = U >= threshold;
+    always @(posedge clk) begin
+        if (rst_n) begin
+            timer <= 0;
+            U <= 0;
+		end else if (~en) begin
+        end else if (timer == 0
+                && U >= threshold) begin
+            // Change as needed
+            U <= U + I - threshold;
+            timer <= 4'd15;
+        end else begin
+            U <= (U >> 1) + I;
+            if (timer != 0)
+                timer <= timer - 1;
+        end
+    end
+endmodule
--- src/tt_um_project.v
+++ src/tt_um_project.v
@@ -18,10 +18,17 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = 8'h0;
+  assign uio_oe = 8'h1;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  reg [22:0] div;
+  always @(posedge clk) begin
+    div <= div + 1;
+  end
+
+  reg [7:0] U;
+  lif lif1(.clk(clk), .I(ui_in), .rst_n(~rst_n), .en(&div), .spike(uo_out[0]));
+  assign uo_out[7:1] = {7{uo_out[0]}};
 
 endmodule
